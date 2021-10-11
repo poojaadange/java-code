3c50cd37b36f333451548e86dda4494a6be6c9e4
poojaadange <92299463+poojaadange@users.noreply.github.com>
2021-10-11 14:13:09 +0530

    Add files via upload

---
+++ assignment1.java
@@ -0,0 +1,15 @@
+public class assignment1 {
+    public static void main(String[] args) {
+        int a = 100, b = 12, c = 65;
+    if(a > b && a > c){
+        System.out.println("A is Greatest");
+    }
+    else if(b > a && b > c){
+        System.out.println("B is Greatest");
+    }
+    else{
+        System.out.println("C is Greatest");
+    }    
+    }
+}
+
